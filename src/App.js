f6f930ebec2f9475b1e2aa7221f2e770b8eebe5c
Sam Coe <coe6@purdue.edu>
2018-05-29 10:28:46 -0400

    Styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { Route, Switch, NavLink } from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -10,9 +10,18 @@ class App extends Component {
           <h3>Ain't no party like an</h3>
           <h1>API Party</h1>
         </div>
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/github" render={() => <h1>GitHub!</h1>} />
+          <Route render={() => <p>To get started, click one of the links above.</p>} />
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
