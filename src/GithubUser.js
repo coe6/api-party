52e99150192551e6b0684d1a0df352d0dde1800e
Sam Coe <coe6@purdue.edu>
2018-05-29 11:52:53 -0400

    Make fetch work

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 
 class GithubUser extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            user: {}
+        }
+
+        this.fetchUserData(this.props)
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const locationChanged = nextProps.location !== this.props.location
+        if(locationChanged) {
+            this.fetchUserData(nextProps)
+        }
+    }
+
+    fetchUserData = (props) => {
+        fetch(`https://api.github.com/users/${props.match.params.username}`)
+            .then(response => response.json())
+            .then(user => this.setState({ user }))
+            .catch((() => console.log("error")))
+    }
+
     render() {
+        const { user } = this.state
         return (
             <div className="GithubUser">
-                <h1>Github User: {this.props.match.params.username}</h1>
+                <img src={user.avatar_url} alt=""/>
+                <h2>{user.login}</h2>
+                <h3>Followers: {user.followers}</h3>
+                <h3>Following: {user.following}</h3>
+                <h3>Location: {user.location}</h3>
+                <a href={user.html_url} target="_blank">{user.login}'s profile</a>
             </div>
         )
     }
